b5de11cd0ea65a1a349b2befb93e355510a7b458
Johnson <johnsonlee1126@163.com>
2020-10-15 17:20:56 +0800

    update

--- public/api/list.js
+++ public/api/list.js
@@ -2,9 +2,10 @@ const fs = require('fs')
 
 module.exports = (req, res) => {
   const filesList = fs.readdirSync('../images', { encoding: 'utf-8' })
-
-  res.status(200).send(filesList.map(item => ({
+  const result = filesList.map(item => ({
     filename: item,
     path: `/images/${item}`
-  })))
+  }))
+
+  res.json(result)
 }
