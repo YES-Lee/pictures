9951185ffc6b2d1f87c50eab6309793177c0dfb4
Johnson <johnsonlee1126@163.com>
2020-10-15 18:08:12 +0800

    v0.0.1

--- api/list.js
+++ api/list.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (req, res) => {
-  const filesList = fs.readdirSync('../images', { encoding: 'utf-8' })
+  const filesList = fs.readdirSync(path.resolve(__dirname, '../public/images'), { encoding: 'utf-8' })
   const result = filesList.map(item => ({
     filename: item,
     path: `/images/${item}`
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="picture-wrapper">
-    <ul>
-      <li v-for="image of imageList" :key="image.name">
-        {{image.path}}
+    <ul class="image-list">
+      <li class="image-item" v-for="image of imageList" :key="image.name">
+        <img class="image-item__image" :src="image.path" alt="image.filename">
+        <div class="mask">
+          <button class="view" @click="handleView(image)">查看</button>
+          <button class="view" @click="handleCopy(image)">拷贝</button>
+        </div>
       </li>
     </ul>
   </div>
@@ -22,9 +26,54 @@ export default {
     Axios.get('/api/list').then(result => {
       this.imageList = result.data
     })
+  },
+  methods: {
+    handleView (image) {
+      window.open(image.path, '_blank')
+    },
+    handleCopy (image) {
+      const url = window.location.origin + image.path
+      window.navigator.clipboard.writeText(url).then(() => {
+        window.alert('地址已拷贝')
+      }).catch(() => {
+        window.alert('拷贝失败，请手动复制：' + url)
+      })
+    }
   }
 }
 </script>
 
 <style lang="less">
+.image-list {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+
+  .image-item {
+    display: block;
+    margin-left: 10px;
+    border-radius: 4px;
+    overflow: hidden;
+    position: relative;
+    &__image {
+      display: block;
+      width: 128px;
+      height: 128px;
+    }
+
+    .mask {
+      position: absolute;
+      background: rgba(0,0,0,0.3);
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      display: none;
+    }
+
+    &:hover .mask {
+      display: block;
+    }
+  }
+}
 </style>
