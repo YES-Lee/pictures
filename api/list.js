750cfc98574f6ea36a6af3c46fbe39d2fd949aa9
Johnson <johnsonlee1126@163.com>
2020-10-16 09:25:32 +0800

    fix: api url

--- api/list.js
+++ api/list.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = (req, res) => {
-  const filesList = fs.readdirSync(path.resolve(__dirname, '../public/pictures'), { encoding: 'utf-8' })
+  const filesList = fs.readdirSync(path.resolve(__dirname, '../pictures'), { encoding: 'utf-8' })
   const result = filesList.map(item => ({
     filename: item,
     path: `/pictures/${item}`
